25ac0a1521864e8480fa4c2d6c2d2e7388c4d0e7
TRanDUc8386Duc <quangdao25qq@gmail.com>
2025-05-15 22:06:57 +0700

    update v2

--- src/main/webapp/js/price.js
+++ src/main/webapp/js/price.js
@@ -4,9 +4,48 @@ $(document).ready(function() {
 
         $('#price-range-submit').show();
 
-        var min_price_range = parseInt($("#min_price").val());
-
-        var max_price_range = parseInt($("#max_price").val());
+        var uls = new URL(document.URL)
+    var id = uls.searchParams.get("id");
+    if (id != null) {
+        var url = 'http://localhost:8080/api/product-comment/public/find-by-product?idproduct=' + id;
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: new Headers({
+                'Authorization': 'Bearer ' + token
+            })
+        });
+        var list = await response.json();
+        var main = ''
+        for (i = 0; i < list.length; i++) {
+            var listImg = ''
+            for (j = 0; j < list[i].productCommentImages.length; j++) {
+                listImg += `<img class="imgcomment" src="${list[i].productCommentImages[j].linkImage}">`
+            }
+            var star = '';
+            for (j = 0; j < list[i].star; j++) {
+                star += `<span class="fa fa-star checkedstar"></span>`
+            }
+            main += `<div class="singlectlct">
+            <div class="row">
+                <div class="col-11">
+                    <div class="d-flex nguoidangctl">
+                        <img class="avtuserdangctl" src="image/avatar.webp">
+                        <span class="usernamedangctl">${list[i].user.fullname==null?'Người dùng':list[i].user.fullname}</span>
+                        <span class="ngaytraloi">${list[i].createdDate}</span>
+                        <span class="starcmts">${star}</span>
+                        ${list[i].isMyComment==true?`<span class="starcmts"><i onclick="deleteComment(${list[i].id})" class="fa fa-trash pointer"></i></span>`:''}
+                    </div>
+                    <div class="contentctlct">${list[i].content}</div>
+                    <div class="listimgcontent">
+                        ${listImg}
+                    </div>
+                </div>
+            </div>
+        </div>`
+        }
+        document.getElementById("listcautlct").innerHTML = main
+    }
+}
 
         if (min_price_range > max_price_range) {
             $('#max_price').val(min_price_range);
